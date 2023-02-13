c09349419b43564cf595708867223386c0920d16
Sam Tanner <samuel.m.s.tanner@gmail.com>
2023-02-13 13:47:19 -0800

    reogranizes app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://use.typekit.net/tmm0job.css" />
+        <link rel="stylesheet" href="https://use.typekit.net/tmm0job.css" />
+        <title>Sam Tanner</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
