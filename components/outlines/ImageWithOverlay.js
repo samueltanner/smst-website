8e05b8e8a1f990f0e28421869cd5331c41a15a37
samueltanner <samuel.m.s.tanner@gmail.com>
2023-02-17 13:17:00 -0800

    gets about me section working

--- components/outlines/ImageWithOverlay.js
+++ components/outlines/ImageWithOverlay.js
@@ -3,7 +3,13 @@ import Image from 'next/image'
 export const ImageWithOverlay = ({ src, children, className }) => {
   return (
     <div className="relative flex h-[80%] w-full">
-      <Image src={src} alt="Sam Tanner" fill priority className={className} />
+      <Image
+        src={src}
+        alt="Sam Tanner"
+        fill
+        className={className}
+        sizes="(max-width: 768px) 100vw, 768px"
+      />
       {children}
     </div>
   )
