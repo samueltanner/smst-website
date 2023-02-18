f07448c49cb3a8f0cfe8020707eac493e5add75d
samueltanner <samuel.m.s.tanner@gmail.com>
2023-02-17 16:35:11 -0800

    makes about me section

--- components/outlines/ImageWithOverlay.js
+++ components/outlines/ImageWithOverlay.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export const ImageWithOverlay = ({ src, children, className }) => {
   return (
-    <div className="relative flex h-[80%] w-full">
+    <div className="relative flex h-full w-full m-4">
       <Image
         src={src}
         alt="Sam Tanner"
