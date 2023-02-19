56dac17b5325bd6edfb08eeb5e8b79e12e2baf0e
samueltanner <samuel.m.s.tanner@gmail.com>
2023-02-18 20:06:43 -0800

    [WIP] flipping cards

--- components/PortfolioCard.js
+++ components/PortfolioCard.js
@@ -1,25 +1,44 @@
 import Image from 'next/image'
 import { FiArrowRight } from 'react-icons/fi'
-export const PortfolioCard = ({ project }) => {
-  return (
-    <div className="group h-[400px] w-[80%] min-w-[300px] rounded-lg border-[4px] border-primary bg-offWhite text-primary drop-shadow-lg md:w-[45%]">
-      <div className="absolute z-10 flex h-full w-full items-center justify-center bg-zinc-900 bg-opacity-50 ">
-        <span className="flex h-full w-full items-center gap-2 md:justify-center">
-          <p className="ml-8 w-1/2 font-primary text-2xl font-bold text-white md:w-fit">
-            {project.title}
-          </p>
-          <span className="flex h-full items-center justify-center">
-            <FiArrowRight className="mt-1 h-6 w-6 stroke-[3px] text-white opacity-0  transition-opacity duration-300 ease-in-out group-hover:opacity-100" />
-          </span>
-        </span>
-      </div>
+import { useEffect, useState } from 'react'
+export const PortfolioCard = ({ project, index, selectedCard }) => {
+  const [cardBackVisible, setCardBackVisible] = useState(false)
+  useEffect(() => {
+    const backgroundTimeout = () => {
+      setTimeout(() => {
+        // setCardBackVisible(true)
+      }, 350 / 2)
+    }
+    // if (selectedCard && selectedCard === index) {
+    //   backgroundTimeout()
+    // } else {
+    // }
 
-      <Image
-        src={project.images[0]}
-        fill
-        className="object-cover"
-        alt={project.title}
-      />
+    return () => backgroundTimeout()
+  }, [selectedCard, index])
+  return (
+    <div className="group h-full w-full rounded-lg border-[4px] border-primary bg-offWhite text-primary drop-shadow-lg ">
+      {!cardBackVisible && (
+        <>
+          <div className="absolute z-10 flex h-full w-full items-center justify-center bg-zinc-900 bg-opacity-50 ">
+            <span className="flex h-full w-full items-center gap-2 md:justify-center">
+              <p className="ml-8 w-1/2 font-primary text-2xl font-bold text-white md:w-fit">
+                {project.title}
+              </p>
+
+              <span className="flex h-full items-center justify-center">
+                <FiArrowRight className="mt-1 h-6 w-6 stroke-[3px] text-white opacity-0  transition-opacity duration-300 ease-in-out group-hover:opacity-100" />
+              </span>
+            </span>
+          </div>
+          <Image
+            src={project.images[0]}
+            fill
+            className="object-cover"
+            alt={project.title}
+          />
+        </>
+      )}
     </div>
   )
 }
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,13 +1,63 @@
 import { Header } from '../components/Header'
 import { PortfolioCard } from '../components/PortfolioCard'
 import { portfolio_data } from '../lib/data'
+import { motion } from 'framer-motion'
+import { useState } from 'react'
+
 export default function Portfolio() {
+  const [selectedCard, setSelectedCard] = useState(false)
+
+  const cardVariants = {
+    selected: {
+      rotateY: 180,
+      // x: '50%',
+      scale: 1,
+      transition: { duration: 0.35, delay: 0 },
+      zIndex: 20,
+      boxShadow:
+        'rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+    },
+    notSelected: (i) => ({
+      rotateY: i * 15,
+      scale: 1 - Math.abs(i * 0.15),
+      x: i ? i * 50 : 0,
+      zIndex: 10 - Math.abs(i),
+      boxShadow:
+        'rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px',
+      transition: { duration: 0.35 },
+    }),
+  }
+
+  const handleSelectCard = (index) => {
+    if (selectedCard === index) {
+      setSelectedCard(null)
+    } else {
+      setSelectedCard(index)
+    }
+  }
   return (
     <div className="flex h-screen w-screen flex-col overflow-y-scroll">
       <Header sticky={true} />
       <div className="flex w-full flex-wrap justify-center gap-8 py-8">
         {portfolio_data.map((project, index) => (
-          <PortfolioCard key={index} project={project} />
+          <motion.div
+            key={index}
+            className={`h-[400px] w-[80%] min-w-[300px] md:w-[45%] ${
+              selectedCard === index ? '' : ''
+            }`}
+            variants={cardVariants}
+            animate={selectedCard === index ? 'selected' : 'notSelected'}
+            custom={selectedCard === index ? selectedCard : 0}
+            onClick={() => {
+              handleSelectCard(index)
+            }}
+          >
+            <PortfolioCard
+              project={project}
+              index={index}
+              selectedCard={selectedCard}
+            />
+          </motion.div>
         ))}
       </div>
     </div>
