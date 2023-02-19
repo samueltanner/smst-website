dcbe3e731ddd37018084ca7bf696e2416927c15a
samueltanner <samuel.m.s.tanner@gmail.com>
2023-02-19 09:51:12 -0800

    fudging around on portfolio

--- components/PortfolioCard.js
+++ components/PortfolioCard.js
@@ -2,43 +2,25 @@ import Image from 'next/image'
 import { FiArrowRight } from 'react-icons/fi'
 import { useEffect, useState } from 'react'
 export const PortfolioCard = ({ project, index, selectedCard }) => {
-  const [cardBackVisible, setCardBackVisible] = useState(false)
-  useEffect(() => {
-    const backgroundTimeout = () => {
-      setTimeout(() => {
-        // setCardBackVisible(true)
-      }, 350 / 2)
-    }
-    // if (selectedCard && selectedCard === index) {
-    //   backgroundTimeout()
-    // } else {
-    // }
-
-    return () => backgroundTimeout()
-  }, [selectedCard, index])
   return (
-    <div className="group h-full w-full rounded-lg border-[4px] border-primary bg-offWhite text-primary drop-shadow-lg ">
-      {!cardBackVisible && (
-        <>
-          <div className="absolute z-10 flex h-full w-full items-center justify-center bg-zinc-900 bg-opacity-50 ">
-            <span className="flex h-full w-full items-center gap-2 md:justify-center">
-              <p className="ml-8 w-1/2 font-primary text-2xl font-bold text-white md:w-fit">
-                {project.title}
-              </p>
+    <div className="group h-full w-full rounded-lg border-[4px] border-primary bg-offWhite text-primary drop-shadow-lg transition duration-200 ease-in-out hover:scale-105 hover:transform">
+      <div className="absolute z-10 flex h-full w-full cursor-pointer items-center justify-center bg-zinc-900 bg-opacity-50 ">
+        <span className="flex h-full w-full items-center gap-2 md:justify-center">
+          <p className="ml-8 w-full font-primary text-2xl font-bold text-white md:w-fit">
+            {project.title}
+          </p>
 
-              <span className="flex h-full items-center justify-center">
-                <FiArrowRight className="mt-1 h-6 w-6 stroke-[3px] text-white opacity-0  transition-opacity duration-300 ease-in-out group-hover:opacity-100" />
-              </span>
-            </span>
-          </div>
-          <Image
-            src={project.images[0]}
-            fill
-            className="object-cover"
-            alt={project.title}
-          />
-        </>
-      )}
+          <span className="mr-8 flex h-full items-center justify-center">
+            <FiArrowRight className="mt-0.5 h-6 w-6 stroke-[3px] text-white opacity-0  transition-opacity duration-300 ease-in-out group-hover:opacity-100" />
+          </span>
+        </span>
+      </div>
+      <Image
+        src={project.images[0]}
+        fill
+        className="object-cover"
+        alt={project.title}
+      />
     </div>
   )
 }
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -7,27 +7,6 @@ import { useState } from 'react'
 export default function Portfolio() {
   const [selectedCard, setSelectedCard] = useState(false)
 
-  const cardVariants = {
-    selected: {
-      rotateY: 180,
-      // x: '50%',
-      scale: 1,
-      transition: { duration: 0.35, delay: 0 },
-      zIndex: 20,
-      boxShadow:
-        'rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-    },
-    notSelected: (i) => ({
-      rotateY: i * 15,
-      scale: 1 - Math.abs(i * 0.15),
-      x: i ? i * 50 : 0,
-      zIndex: 10 - Math.abs(i),
-      boxShadow:
-        'rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px',
-      transition: { duration: 0.35 },
-    }),
-  }
-
   const handleSelectCard = (index) => {
     if (selectedCard === index) {
       setSelectedCard(null)
@@ -45,9 +24,6 @@ export default function Portfolio() {
             className={`h-[400px] w-[80%] min-w-[300px] md:w-[45%] ${
               selectedCard === index ? '' : ''
             }`}
-            variants={cardVariants}
-            animate={selectedCard === index ? 'selected' : 'notSelected'}
-            custom={selectedCard === index ? selectedCard : 0}
             onClick={() => {
               handleSelectCard(index)
             }}
