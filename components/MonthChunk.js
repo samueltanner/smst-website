c0d39189731872eaf3ed1a43e2f3783365d75aeb
samueltanner <samuel.m.s.tanner@gmail.com>
2023-02-19 21:07:16 -0800

    gets events in the right place

--- components/MonthChunk.js
+++ components/MonthChunk.js
@@ -1,12 +1,19 @@
 export const MonthChunk = ({ year, month, data }) => {
   return (
     <div
-      className=""
+      className="flex flex-col  text-center"
       onClick={() => {
         console.log(year, month, data)
       }}
     >
-      •
+      {!!data.length && (
+        <div className="mb-20 flex h-20 flex-col items-center">
+          <span className="h-10 w-10 flex-none rounded-full border-2 border-primary" />
+          <span className="h-full flex-none border-l-2 border-primary" />
+          <span className="h-2 w-2 flex-none rounded-full bg-primary" />
+        </div>
+      )}
+      {/* <p>•</p> */}
     </div>
   )
 }
--- pages/resume.js
+++ pages/resume.js
@@ -2,46 +2,8 @@ import { Header } from '../components/Header'
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import { MonthChunk } from '../components/MonthChunk'
-// import { resume_data } from '../lib/data'
+import { resume_data } from '../lib/data'
 import dayjs from 'dayjs'
-
-const resume_data = {
-  work: [
-    {
-      name: 'CardSwapper',
-      position: 'Co-Founder & CEO',
-      startDate: '2013-02-28',
-      endDate: '2016-06-30',
-      highlights: [],
-      summary:
-        "The world's first peer-to-peer trading platform for unused and unwanted gift cards\n\n•Co-founded business as an undergraduate freshman at the University of Washington\n•Designed and wire-framed application \n•Hired and managed team to develop, and launch application on iOS app store\n•Raised seed funding from Jones + Foster Accelerator and from private investors\n•Wrote, submitted, and received patent from the \nUnited States Patent and Trademark Office for novel methods of digital gift card exchange \n•Partnered with nations largest gift card liquidation platform to launch cash-for-cards feature",
-      url: 'https://www.linkedin.com/company/cardswapr/',
-      location: 'Seattle, Washington, United States',
-    },
-    {
-      name: 'University of Washington - School of Music',
-      position: 'Student Visual and Audio Archiver',
-      startDate: '2013-10-31',
-      endDate: '2016-05-31',
-      highlights: [],
-      summary:
-        '•Recorded, live-mixed, and produced live classical, jazz, choral, and orchestral performances in high-fidelity\n•Self-taught Logic Pro, Audacity, and Premiere Pro to increase quality of archived media \n•Hired independently by graduate students to produce thesis performances due to quality of work',
-      url: 'https://www.linkedin.com/school/uw-school-of-music/',
-      location: 'Seattle, Washington, United States',
-    },
-    {
-      name: 'Tango Card, Inc.',
-      position: 'Social Networking Intern',
-      startDate: '2013-06-30',
-      endDate: '2013-09-30',
-      highlights: [],
-      summary:
-        '•Managed Facebook, Twitter, and Linkedin social profiles via Hootsuite \n•Deployed social marketing strategies focused on increasing relevance amongst key demographics\n•Nearly doubled Twitter following using unpaid-marketing strategies',
-      url: 'https://www.linkedin.com/company/tango-card-inc/',
-      location: 'Seattle, Washington, United States',
-    },
-  ],
-}
 
 const years = [2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013]
 const months = [
@@ -77,8 +39,8 @@ export default function Resume() {
     <div className="flex h-screen w-screen flex-col">
       <Header sticky={true} />
       <div
-        className="flex h-full w-screen items-end justify-start
-         gap-1 overflow-y-scroll px-6 pb-20"
+        className="flex h-full w-screen
+         gap-1 overflow-y-scroll px-20 pb-20"
       >
         {years.map((year) => {
           return (
@@ -96,7 +58,7 @@ export default function Resume() {
 
 const YearChunk = ({ year, data }) => {
   return (
-    <div className="relative flex h-full w-48 cursor-pointer items-end justify-center border-b-[10px] border-secondary text-start text-6xl font-bold text-primary">
+    <div className="relative flex h-full w-48 cursor-pointer items-end justify-center text-start text-6xl font-bold text-primary">
       <div className="absolute right-0 bottom-12 flex h-full w-full flex-row-reverse items-end justify-between">
         {months.map((month, index) => {
           return (
@@ -113,7 +75,7 @@ const YearChunk = ({ year, data }) => {
           )
         })}
       </div>
-      {year}
+      <p>{year}</p>
     </div>
   )
 }
