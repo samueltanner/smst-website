684b95b817aacc4c3fcc28da6ed27a8df0ce5a89
samueltanner <samuel.m.s.tanner@gmail.com>
2023-02-20 11:22:16 -0800

    mobile view height

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,19 @@ function MyApp({ Component, pageProps }) {
   const [theme, setTheme] = useState('default')
 
   useEffect(() => {
+    const docHeightFix = () => {
+      document?.documentElement.style.setProperty(
+        '--vh',
+        window.innerHeight * 0.01 + 'px'
+      )
+    }
+    window.addEventListener('resize', docHeightFix)
+    return () => {
+      window.removeEventListener('resize', docHeightFix)
+    }
+  })
+
+  useEffect(() => {
     if (!theme) {
       setTheme('default')
     }
